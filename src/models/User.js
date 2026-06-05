4fed617132ebe970dce86176f9bf484d44147893
Shashank Padala <shashankpadala2457@gmail.com>
2026-06-05 23:20:15 +0530

    model change

--- src/models/User.js
+++ src/models/User.js
@@ -55,6 +55,10 @@ const userSchema = new mongoose.Schema(
       type: [String],
       default: [],
     },
+    maxReviewsPerDay: {
+      type: Number,
+      default: 10,
+    },
   },
   {
     timestamps: true,
